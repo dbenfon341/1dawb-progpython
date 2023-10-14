132d48a6d3ca42f41421ff792c8e1d7ce460eff9
dbenfon341 <dbenfon341@g.educaand.es>
2023-10-14 21:01:51 +0200

    Añadido el ejercicio con el código.

---
+++ EjerciciosU1/Ejercicio_1_2.py
@@ -0,0 +1,9 @@
+# Ejercicio 2.2
+# Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio.
+
+
+horas = int(input("Horas de trabajo: "))
+precio = int(input("Coste por hora: "))
+total = horas * precio
+
+print("Importe total:", total)
